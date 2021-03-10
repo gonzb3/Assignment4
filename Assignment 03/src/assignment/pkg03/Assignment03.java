4409480d53f0532cd49527842ded94ce3b204d8b
gonzb3 <Eatingplatano@192.168.1.8>
2021-03-10 05:56:51 -0500

    --added circles

--- Assignment 03/src/assignment/pkg03/Assignment03.java
+++ Assignment 03/src/assignment/pkg03/Assignment03.java
@@ -5,9 +5,9 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.shape.StrokeType;
 import javafx.stage.Stage;
 
 public class Assignment03 extends Application{
@@ -28,6 +28,23 @@ public class Assignment03 extends Application{
         Rectangle rctRed = new Rectangle(810,588,Color.CRIMSON);
         Rectangle rctWhite = new Rectangle (810, 980/14,Color.WHITE);
         Rectangle rctBlackDivider = new Rectangle (810,10,Color.LIGHTGREY);
+        Rectangle rctPilOne = new Rectangle (80,418,Color.WHITE);
+        Rectangle rctPilTwo = new Rectangle (80,418,Color.WHITE);
+        Rectangle rctPilThree = new Rectangle (80,418,Color.WHITE);
+        Rectangle rctPilFour = new Rectangle (80,418,Color.WHITE);
+        Rectangle rctDoor1 = new Rectangle (80, 200, Color.WHITE);
+        Rectangle rctDoor2 = new Rectangle (80, 250, Color.WHITE);
+        Rectangle rctDoor3 = new Rectangle (80, 250, Color.WHITE);
+        Rectangle rctSqr = new Rectangle (100,60, Color.LIGHTGRAY);
+        
+        Circle cirOne = new Circle(); 
+        Circle cirTwo = new Circle(); 
+        Circle cirThree = new Circle();
+        Circle cirFour = new Circle();
+        Circle cirFive = new Circle();
+        Circle cirSix = new Circle();
+        Circle cirSeven = new Circle();
+        Circle cirEight = new Circle();
         
         Polygon plyRoof = new Polygon(
                 1080/2, 0.0,
@@ -45,6 +62,48 @@ public class Assignment03 extends Application{
         rctWhite.setY(980/4);
         rctBlackDivider.setY(980-(980/4));
         rctBlackDivider.setX(1080/8);
+        rctPilOne.setY(980/4+70);
+        rctPilOne.setX(160);
+        rctPilTwo.setY(980/4+70);
+        rctPilTwo.setX(380);
+        rctPilThree.setY(980/4+70);
+        rctPilThree.setX(620);
+        rctPilFour.setY(980/4+70);
+        rctPilFour.setX(840);
+        rctDoor1.setX(265);
+        rctDoor1.setY(533);
+        rctDoor2.setX(500);
+        rctDoor2.setY(483);                
+        rctDoor3.setX(730);
+        rctDoor3.setY(533);
+        rctSqr.setY(380);
+        rctSqr.setX(490);
+        cirOne.setCenterX(162);
+        cirOne.setCenterY(340);
+        cirOne.setRadius(25);
+        cirTwo.setCenterX(235);
+        cirTwo.setCenterY(341);
+        cirTwo.setRadius(25);
+        cirThree.setCenterX(385);
+        cirThree.setCenterY(341);
+        cirThree.setRadius(25);
+        cirFour.setCenterX(455);
+        cirFour.setCenterY(341);
+        cirFour.setRadius(25);
+        cirFive.setCenterX(625);
+        cirFive.setCenterY(341);
+        cirFive.setRadius(25);
+        cirSix.setCenterX(700);
+        cirSix.setCenterY(341);
+        cirSix.setRadius(25);
+        cirSeven.setCenterX(845);
+        cirSeven.setCenterY(341);
+        cirSeven.setRadius(25);
+        cirEight.setCenterX(915);
+        cirEight.setCenterY(341);
+        cirEight.setRadius(25);
+        
+
         
         //setting style
         rctRed.setStroke(Color.BLACK);
@@ -52,20 +111,46 @@ public class Assignment03 extends Application{
         plyRoof.setFill(Color.WHITE);
         plyRoof.setStroke(Color.BLACK);
         rctBlackDivider.setStroke(Color.BLACK);
+        rctPilOne.setStroke(Color.BLACK);
+        rctPilTwo.setStroke(Color.BLACK);
+        rctPilThree.setStroke(Color.BLACK);
+        rctPilFour.setStroke(Color.BLACK);
+        rctDoor1.setStroke(Color.BLACK);
+        rctDoor2.setStroke(Color.BLACK);
+        rctDoor3.setStroke(Color.BLACK);
+        rctSqr.setStroke(Color.BLACK);
+        cirOne.setStroke(Color.BLACK);
+        cirOne.setFill(Color.WHITE);
+        cirTwo.setFill(Color.WHITE);
+        cirTwo.setStroke(Color.BLACK);
+        cirThree.setFill(Color.WHITE);
+        cirThree.setStroke(Color.BLACK);
+        cirFour.setFill(Color.WHITE);
+        cirFour.setStroke(Color.BLACK);
+        cirFive.setFill(Color.WHITE);
+        cirFive.setStroke(Color.BLACK);
+        cirSix.setFill(Color.WHITE);
+        cirSix.setStroke(Color.BLACK);
+        cirSeven.setFill(Color.WHITE);
+        cirSeven.setStroke(Color.BLACK);
+        cirEight.setFill(Color.WHITE);
+        cirEight.setStroke(Color.BLACK);
+        
         
         //groups
-        Group gp1 = new Group(rctBlue,rctGreen,rctRed,rctWhite, plyRoof,rctBlackDivider);
+        Group gp1 = new Group(rctBlue,rctGreen,rctRed,rctWhite, plyRoof,rctBlackDivider,rctPilOne,rctPilTwo,rctPilThree,rctPilFour,rctDoor1,rctDoor2,rctDoor3,rctSqr,cirOne,cirTwo,cirThree,cirFour,cirFive,cirSix,cirSeven,cirEight);
         
         //stairs for loop
        for (int i = 0; i < size; i++){
-           arrRct[i] = new Rectangle(stepsWidth+=i*6,20,Color.LIGHTGRAY);
+           arrRct[i] = new Rectangle(stepsWidth+=i*8,20,Color.LIGHTGRAY);
            arrRct[i].setStroke(Color.BLACK);
            
            if (i == 0){
-           arrRct[i].setY(stepsY);}
-           else {arrRct[i].setY(stepsY+= 15);}
-           
-           arrRct[i].setX(stepsX -= i * 3);           
+           arrRct[i].setY(stepsY);
+           arrRct[i].setX(stepsX);}
+           else {arrRct[i].setY(stepsY+= 15);
+                 arrRct[i].setX(stepsX -= i * 4); }
+               
            gp1.getChildren().add(arrRct[i]);
        }
         
