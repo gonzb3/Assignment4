0d485b05804701c14a9456b9fbea72933de8c0c3
gonzb3 <Eatingplatano@192.168.1.8>
2021-03-10 03:02:43 -0500

    --added javaFx Rectangles, Polygons , And Styles

--- Assignment 03/src/assignment/pkg03/Assignment03.java
+++ Assignment 03/src/assignment/pkg03/Assignment03.java
@@ -5,7 +5,9 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.StrokeType;
 import javafx.stage.Stage;
 
 public class Assignment03 extends Application{
@@ -19,19 +21,43 @@ public class Assignment03 extends Application{
     public void start(Stage stage) throws Exception {
 
         
+        //Setting variable 
+        Rectangle rctBlue = new Rectangle(1080, 980/2, Color.LIGHTBLUE);
+        Rectangle rctGreen = new Rectangle (1080, 980/2, Color.GREEN);                
+        Rectangle rctRed = new Rectangle(810,588,Color.CRIMSON);
+        Rectangle rctWhite = new Rectangle (810, 980/14,Color.WHITE);
+        Rectangle rctBlackDivider = new Rectangle (810,10,Color.LIGHTGREY);
         
-        Rectangle rctBlue = new Rectangle(1080, 980/3, Color.LIGHTBLUE);
-        Rectangle rctGreen = new Rectangle (1080, (980/3)*2, Color.GREEN);
-        rctGreen.setY(980/3);
+        Polygon plyRoof = new Polygon(
+                1080/2, 0.0,
+                1080/18, 980/4,
+                1020, 980/4);
+ 
         
         
+        //Setting positions 
+        rctGreen.setY(980/2);
+        rctRed.setY(980/4);
+        rctRed.setX(1080/8);
+        rctWhite.setX(1080/8);
+        rctWhite.setY(980/4);
+        rctBlackDivider.setY(980-(980/4));
+        rctBlackDivider.setX(1080/8);
         
+        //setting style
+        rctRed.setStroke(Color.BLACK);
+        rctWhite.setStroke(Color.BLACK);
+        plyRoof.setFill(Color.WHITE);
+        plyRoof.setStroke(Color.BLACK);
+        rctBlackDivider.setStroke(Color.BLACK);
         
-        Group gp1 = new Group(rctBlue,rctGreen );
-        Group gp2 = new Group();
+        //groups
+        Group gp1 = new Group(rctBlue,rctGreen,rctRed,rctWhite, plyRoof,rctBlackDivider);
         
+        //scenes 
         Scene sn = new Scene(gp1, 1080, 980);
         
+        //stage
         stage.setTitle("A House");
         stage.setScene(sn);
         stage.show();
